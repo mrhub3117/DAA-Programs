11751087122278dfd84d4928502195daf1b02c7f
mrhub3117 <pathan77000@gmail.com>
2025-05-13 23:36:47 +0530

    Create Quicksort.java

---
+++ Quicksort.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Quicksort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; 
+        int i = low - 1; 
+        
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        
+        System.out.println("Enter " + n + " integer elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        scanner.close();  
+
+        
+        quickSort(arr, 0, arr.length - 1);
+
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
